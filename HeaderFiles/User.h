4a8c066908732915e859f477738a364f189a79f6
ahmed-kashkoush <ahmedkashkoush464@gmail.com>
2023-03-03 21:38:47 +0200

    Implementing Basic Features of User class

--- HeaderFiles/User.h
+++ HeaderFiles/User.h
@@ -4,5 +4,29 @@
 
 #ifndef ONLINEBOOKREADER_USER_H
 #define ONLINEBOOKREADER_USER_H
+#include "UtilitieFunctions.h"
+class User{
+private:
+    string Username, Password,
+    Name, Email;
+    bool Admin;
+public:
+    User();
+    User(const string&, const string&, const string&, const string&);
+    // Getters and setters
+    const string &GetUsername()const;
+    const string &GetPassword()const;
+    const string &GetName()const;
+    const string &GetEmail()const;
+    void SetUsername(const string&);
+    void SetPassword(const string&);
+    void SetName(const string&);
+    void SetEmail(const string&);
 
+    // functions used By UserManager
+    void PrintProfile()const;
+    void EnterUser(const string &);
+
+
+};
 #endif //ONLINEBOOKREADER_USER_H
