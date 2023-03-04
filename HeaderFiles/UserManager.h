d1dbc698f13edbae753598bd2ec5a5ea7f44b921
ahmed-kashkoush <ahmedkashkoush464@gmail.com>
2023-03-04 14:52:10 +0200

    Implementing Basic Features of Managers

--- HeaderFiles/UserManager.h
+++ HeaderFiles/UserManager.h
@@ -4,5 +4,23 @@
 
 #ifndef ONLINEBOOKREADER_USERMANAGER_H
 #define ONLINEBOOKREADER_USERMANAGER_H
+#include "UtilitieFunctions.h"
+#include "Book.h"
+#include "User.h"
+class UserManager{
+private:
+    vector<User> Users;
+    User Current;
+    map<string, User> UsernameToUser;
+    void Login();
+    void Signup();
 
+public:
+
+    void AccessSystem();
+    User GetCurrent();
+
+
+
+};
 #endif //ONLINEBOOKREADER_USERMANAGER_H
--- SourceFiles/UserManager.cpp
+++ SourceFiles/UserManager.cpp
@@ -3,3 +3,45 @@
 //
 
 #include "../HeaderFiles/UserManager.h"
+
+void UserManager::AccessSystem() {
+    int Choice=Utility::ShowRegestrationMenu();
+    if(Choice==1)Login();
+    else        Signup();
+    Current.SayHello();
+}
+
+User UserManager::GetCurrent() {
+    return Current;
+}
+
+void UserManager::Login() {
+        cout<<"Enter Username: ";
+        string username;cin>>username;
+        cout<<"Enter Password ";
+        string password; cin>>password;
+        if(!UsernameToUser.count(username)){
+            cout<<"Invalid Username or Password Please Try Again\n";
+            Login();
+        }else if(UsernameToUser[username].GetPassword()!=password){
+            cout<<"Invalid Username or Password Please Try Again\n";
+            Login();
+        }else{
+            Current=UsernameToUser[username];
+        }
+}
+
+void UserManager::Signup() {
+        cout<<"Enter Username: ";
+        string username;cin>>username;
+        if(UsernameToUser.count(username)){
+            cout<<"Username already exists try Again\n";
+            Signup();
+        }else{
+            Current.EnterUser(username);
+            Users.emplace_back(Current);
+            UsernameToUser[username]=Current;
+        }
+}
+
+
