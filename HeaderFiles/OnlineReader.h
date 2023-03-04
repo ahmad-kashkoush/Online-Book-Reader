b8c5beaf4de9339c6ce866bf9356ab5670641aff
ahmed-kashkoush <ahmedkashkoush464@gmail.com>
2023-03-04 14:53:08 +0200

    Typing Extension and Implementing Run Function

--- HeaderFiles/OnlineReader.h
+++ HeaderFiles/OnlineReader.h
@@ -4,5 +4,17 @@
 
 #ifndef ONLINEBOOKREADER_ONLINEREADER_H
 #define ONLINEBOOKREADER_ONLINEREADER_H
+#include "UtilitieFunctions.h"
+#include "Book.h"
+#include "User.h"
+#include "BookManager.h"
+#include "UserManager.h"
+class OnlineReader{
+private:
+    UserManager Usr;
+    BookManager Bok;
+public:
+    void Run();
 
+};
 #endif //ONLINEBOOKREADER_ONLINEREADER_H
--- SourceFiles/OnlineReader.cpp
+++ SourceFiles/OnlineReader.cpp
@@ -3,3 +3,24 @@
 //
 
 #include "../HeaderFiles/OnlineReader.h"
+
+void OnlineReader::Run() {
+        Usr.AccessSystem();
+        // Admin or not
+        bool isUserAdmin=Usr.GetCurrent().GetStatus();
+        int Choice;
+        while(true){
+            if(isUserAdmin)
+                Choice=Utility::ShowAdminViewMenu();
+            else
+                Choice=Utility::ShowUserViewMenu();
+            if(Choice==1)Usr.GetCurrent().PrintProfile();
+            else if(Choice==2);//lablabla
+            else if(Choice==3)Bok.PrintBooks();
+            else if(Choice==4){
+                cout<<"loggin out....\n";
+                Choice=Utility::ShowRegestrationMenu();
+            }
+
+        }
+}
